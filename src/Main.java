b5f697aeaf5f97c2039899dca522e3b058131166
Dmitriy Kyrov <kdg149@yandex.ru>
2022-07-31 19:26:20 +0300

    Main & READMY changes

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] sales = {10, 20, 30, 0, -5, 45, 5};
+
+        for (int i = 0; i < sales.length; i++) {
+            System.out.print(sales[i] + ", ");
+        }
+        System.out.println("\n" + Arrays.stream(sales).max());
+    }
 }
