40be255f8238c851c302659d7ae79341de1acfd0
Dmitriy Kyrov <kdg149@yandex.ru>
2022-08-03 19:32:50 +0300

    added average method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {10, 20, 30, 0, -5, 45, 5};
+
+        int[] sales = {10, 20, 30, 100, 7, 45, 5};
+
+        SalesManager salesManager = new SalesManager(sales);
+
+        int max = salesManager.max();
+        int min = salesManager.min();
+        int sum = salesManager.sum();
 
         for (int i = 0; i < sales.length; i++) {
             System.out.print(sales[i] + ", ");
         }
-        System.out.println("\n" + Arrays.stream(sales).max());
+        System.out.println("\n Максимальный: " + max);
+        System.out.println("Средний " +
+                "без учета минимального " +
+                "и макимального: " + (sum - max - min) / (sales.length - 2));
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,22 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = max();
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
 }
